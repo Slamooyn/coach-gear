144592713a040693c98b32664667c9a34e5e7592
Slamooyn <leparhoy@gmail.com>
2025-09-07 18:41:32 +0700

    Last commit

--- main/models.py
+++ main/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
 class Product(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=100)
     price = models.IntegerField()
     description = models.TextField()
     thumbnail = models.URLField()
-    category = models.CharField()
+    category = models.CharField(max_length=50)
     is_featured = models.BooleanField(default=False)
+    stock = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name
