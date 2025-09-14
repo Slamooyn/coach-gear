c3261cef3403568201864fd4d25152ec694a09d3
Slamooyn <leparhoy@gmail.com>
2025-09-14 10:02:30 +0700

    Tugas 3

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from main.views import add_employee,show_main
+from main.views import show_main,show_json,show_json_by_id,show_xml,show_xml_by_id,product_add,product_details
 
 app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
+    path('json/', show_json, name='show_json'),
+    path('xml/', show_xml, name='show_xml'),
+    path('xml/<str:product_id>/', show_xml_by_id, name='show_xml_by_id'),
+    path('json/<str:product_id>/', show_json_by_id, name='show_json_by_id'),
+    path('product/<str:id>/', product_details, name='product_details'),
+    path('product-add/', product_add, name='product_add'),
 ]
