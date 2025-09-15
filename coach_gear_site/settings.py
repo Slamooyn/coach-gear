432a6f9027bee17b4690ea6dc3da51fa8592cda8
Slamooyn <leparhoy@gmail.com>
2025-09-15 23:54:18 +0700

    test css

--- coach_gear_site/settings.py
+++ coach_gear_site/settings.py
@@ -143,7 +143,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-
+STATIC_ROOT = BASE_DIR / "staticfiles"  # buat collectstatic
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
