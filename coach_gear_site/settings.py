e0b3fb023dd5d2fe88f432bde6850222482543c1
Slamooyn <leparhoy@gmail.com>
2025-09-17 16:14:45 +0700

    Tugas 3 Kelar

--- coach_gear_site/settings.py
+++ coach_gear_site/settings.py
@@ -33,7 +33,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1","muhammad-salman42-coachgear.pbp.cs.ui.ac.id"]
 CSRF_TRUSTED_ORIGINS = [
-    "https://pbp.cs.ui.ac.id/muhammad.salman42/coachgear"
+    "https://muhammad-salman42-coachgear.pbp.cs.ui.ac.id"
 ]
 
 # Application definition
